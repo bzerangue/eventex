5c9dd2c47627bb55f3c4d6e69e8a0a8c79d323a9
Jonas Coch <jonas@klaftertief.de>
2012-08-06 14:32:14 +0200

    Updated readme and added meta information

--- extension.driver.php
+++ extension.driver.php
@@ -1,16 +1,4 @@
 <?php
 
-	Class extension_EventEx extends Extension{
-
-		public function about(){
-			return array('name' => 'EventEx: multiple section form submission',
-						 'version' => '1.01',
-						 'release-date' => '2009-01-21',
-						 'author' => array('name' => 'Your Hero Paul',
-										   'website' => 'http://github.com/yourheropaul/',
-										   'email' => 'paul@behindthelittledoor.co.uk')
-				 		);
-		}
-		
+	Class extension_EventEx extends Extension {
 	}
-		
